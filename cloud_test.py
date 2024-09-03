66fb511dd704d67787e7c69202b2d48eb60cb0d8
jinyoung <jinyoung0309@hansung.ac.kr>
2024-09-04 06:06:26 +0900

    ...

--- cloud_test.py
+++ cloud_test.py
@@ -2,11 +2,11 @@ import subprocess
 
 # `jinyoung` 브랜치에서 frame_capture.py 실행
 subprocess.run(['git', 'checkout', 'jinyoung'], check=True)
-subprocess.run(['python', r"C:\LLM_CONTEST2024\engineering_task\frame_capture.py"], check=True)
+subprocess.run(['python', "C:\LLM_CONTEST2024\engineering_task\frame_capture.py"], check=True)
 print("step1 compledted")
 
 # `minha` 브랜치에서 image_ssim.py 실행
 subprocess.run(['git', 'checkout', 'minha'], check=True)
-subprocess.run(['python', r"C:\LLM_CONTEST2024\engineering_task\image_ssim.py"], check=True)
+subprocess.run(['python', "C:\LLM_CONTEST2024\engineering_task\image_ssim.py"], check=True)
 
 print("Finished processing")
