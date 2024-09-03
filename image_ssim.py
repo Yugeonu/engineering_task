4e081ed6a9689c72c9ba1a2daed7ed43cca63175
jinyoung <jinyoung0309@hansung.ac.kr>
2024-09-04 04:22:26 +0900

    change r

--- image_ssim.py
+++ image_ssim.py
@@ -4,8 +4,8 @@ from skimage.metrics import structural_similarity as ssim
 import numpy as np
 
 # 이미지 폴더 경로 및 결과 파일 경로
-image_folder = r"C:\Users\anjin\PycharmProjects\LLM_Contest\slide_5"
-output_file = r"C:\Users\anjin\OneDrive\바탕 화면\LLM_2024\slide_5_output"
+image_folder = r"C:\Users\anjin\OneDrive\바탕 화면\LLM_CONTEST2024\engineering_task\slide_cloud"
+output_file = r"C:\Users\anjin\OneDrive\바탕 화면\LLM_CONTEST2024\engineering_task\slide_cloud_output"
 
 # 이미지 파일 목록 가져오기
 image_files = [f for f in os.listdir(image_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
